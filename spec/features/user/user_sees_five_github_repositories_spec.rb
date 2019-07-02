386207dccac01987e8b4870b2072342715fad498
Patrick Duvall <duvallp208@gmail.com>
2019-07-02 14:24:31 -0600

    Add spec for user sees 5 github repositories and run migration to add token top users table
    Co-authored-by: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

---
+++ spec/features/user/user_sees_five_github_repositories_spec.rb
@@ -0,0 +1,23 @@
+# As a logged in user
+# When I visit /dashboard
+# Then I should see a section for "Github"
+# And under that section I should see a list of 5 repositories with the name of each Repo linking to the repo on Github
+require "rails_helper"
+
+describe "as a user" do
+  describe "when I visit my dashboard" do
+    it "sees a github section with five repositorie links" do
+      # require "pry"; binding.pry
+      user = create(:user, token: ENV["GITHUB_OAUTH_TOKEN"])
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit dashboard_path
+      within ".github" do
+        expect(page.all(".repository").count).to eq(5)
+        #add more
+      end
+
+    end
+
+  end
+
+end
