0e5b2d5fd3de33f7ed9a2575e0c1870b7ee01885
Patrick Duvall <duvallp208@gmail.com>
2019-07-10 16:54:07 -0600

    Change config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,11 +98,11 @@ Rails.application.configure do
 
   #Set production Active mailer config
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "brownfield-of-sorrow.herokuapp.com"}
     config.action_mailer.smtp_settings = {
       address:              'smtp.sendgrid.net',
       port:                 '587',
       domain:               'brownfield-of-sorrow.herokuapp.com',
-      host:                 "brownfield-of-sorrow.herokuapp.com",
       user_name:            ENV["SENDGRID_USERNAME"],
       password:             ENV["SENDGRID_PASSWORD"],
       authentication:       'plain',
