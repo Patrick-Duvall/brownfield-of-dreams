c7140eb1cf0759169ff618214f95b3c5243b89da
Patrick Duvall <duvallp208@gmail.com>
2019-07-12 13:43:57 -0600

    Add functionality for disallowing a inviting a user with a github but no email

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -9,9 +9,20 @@ class InviteController < ApplicationController
       flash[:failure] = "Invalid Github Handle, please try again"
       render :new
     else
+      email_if_possible(new_user)
+    end
+  end
+
+  private
+  def email_if_possible(new_user)
+    if new_user[:email]
       InviteMailer.invite(current_user.github_name, new_user[:email], params['q']['Github Handle']).deliver_now
       flash[:success] = "Successfully sent invite!"
       redirect_to dashboard_path
+    else
+      flash[:failure] = "That user does not have an email account associated with their github."
+      redirect_to dashboard_path
     end
   end
+
 end
