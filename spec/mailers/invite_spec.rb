4af06df482255c0e26d1319416071e23494039b3
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-12 09:47:58 -0600

    updates invite spec to better test that an email is being sent out

--- spec/mailers/invite_spec.rb
+++ spec/mailers/invite_spec.rb
@@ -4,15 +4,19 @@ RSpec.describe InviteMailer, type: :mailer do
   it "emails an invitation" do
     user = create(:user, github_name: 'tnodland')
     address = 'example@mail.com'
-    expect(ActionMailer::Base.deliveries.count).to eq(0)
-    email = InviteMailer.invite(user.github_name, address, 'testfreind').deliver_now
-    expect(ActionMailer::Base.deliveries.count).to eq(1)
-    expect(email.from).to eq(["sorrow@brownfield.com"])
-    expect(email.to).to eq([address])
-    expect(email.subject).to eq("#{user.github_name} has invited you to our site!")
-    expect(email.body.encoded.include?("<a href=\"http://localhost:3000/register\">")).to eq(true)
 
-    html_email = Capybara.string(email.body.encoded)
+    email = InviteMailer.invite(user.github_name, address, 'testfreind')
+
+    expect { email.deliver_now }.to change { ActionMailer::Base.deliveries.count }.by(1)
+
+    sent_email = email.deliver_now
+    
+    expect(sent_email.from).to eq(["sorrow@brownfield.com"])
+    expect(sent_email.to).to eq([address])
+    expect(sent_email.subject).to eq("#{user.github_name} has invited you to our site!")
+    expect(sent_email.body.encoded.include?("<a href=\"http://localhost:3000/register\">")).to eq(true)
+
+    html_email = Capybara.string(sent_email.body.encoded)
 
     expect(html_email).to have_link("here")
   end
