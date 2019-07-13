bdc220defac8d9f70f264f1bcc1a96ae0fb3ad67
Patrick Duvall <duvallp208@gmail.com>
2019-07-13 09:39:31 -0600

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :friends, only: %i[create]
+  resources :users, only: %i[new create]
+  resources :tutorials, only: %i[show]
+  resources :user_videos, only: %i[create]
+
+  root 'welcome#index'
+  get 'tags/:tag', to: 'welcome#index', as: :tag
+  get '/register', to: 'users#new'
+  get '/invite', to: 'invite#new'
+  post '/invite', to: 'invite#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/auth/:provider/callback', to: 'github/sessions#update'
+  get '/dashboard', to: 'users#show'
+
   namespace :api do
     namespace :v1 do
       resources :tutorials, only: %i[show index]
@@ -8,17 +24,12 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'welcome#index'
-  get 'tags/:tag', to: 'welcome#index', as: :tag
-  get '/register', to: 'users#new'
-
   namespace :admin do
     get '/dashboard', to: 'dashboard#show'
-    resources :tutorials, only: %i[create edit update destroy new] do
+    resources :tutorials, only: %i[create edit update new] do
       resources :videos, only: [:create]
     end
-    resources :videos, only: %i[edit update destroy]
-
+    resources :videos, only: %i[edit update]
     namespace :api do
       namespace :v1 do
         put 'tutorial_sequencer/:tutorial_id', to: 'tutorial_sequencer#update'
@@ -29,28 +40,4 @@ Rails.application.routes.draw do
   namespace :user do
     get '/activation/:user_id', to: 'activation#update', as: 'activation'
   end
-  get '/invite', to: 'invite#new'
-  post '/invite', to: 'invite#create'
-
-  post '/activation' => 'activation#create'
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  get '/auth/:provider/callback', to: 'github/sessions#update'
-
-  get '/dashboard', to: 'users#show'
-  resources :friends, only: [:create]
-
-  # Is this being used?
-  get '/video', to: 'video#show'
-
-  resources :users, only: %i[new create update edit]
-
-  resources :tutorials, only: %i[show index] do
-    resources :videos, only: %i[show index]
-  end
-
-  resources :user_videos, only: %i[create destroy]
 end
